89fba6e898651cca0091dff9774b73f1559de2a1
Andre Vinicius Torres Conrado <vinicius.torresc@gmail.com>
2022-06-20 23:45:23 -0300

    Initial Commit

---
+++ HackerNews/HackerNewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HackerNewsApp.swift
+//  HackerNews
+//
+//  Created by Andre Vinicius Torres Conrado on 20/06/22.
+//
+
+import SwiftUI
+
+@main
+struct HackerNewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
